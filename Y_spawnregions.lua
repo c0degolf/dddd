17549cbe343ee66b7daf7639501dd318fdc5a9ad
Solwoo Song <167087280+c0degolf@users.noreply.github.com>
2025-11-26 22:46:24 +0900

    Add files via upload

---
+++ Y_spawnregions.lua
@@ -0,0 +1,13 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "Rosewood Military Hospital", file = "media/maps/Rosewood Military Hospital/spawnpoints.lua" },
+		{ name = "Trelai_4x4", file = "media/maps/Trelai_4x4/spawnpoints.lua" },
+		{ name = "Trelai_spawns", file = "media/maps/Trelai_spawns/spawnpoints.lua" },
+		{ name = "Trelai_storyspawn", file = "media/maps/Trelai_storyspawn/spawnpoints.lua" },
+		{ name = "RavenCreek", file = "media/maps/RavenCreek/spawnpoints.lua" },
+	}
+end
